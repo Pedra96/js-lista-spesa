5a061410b951b45bb3dcad90bae493de823655b5
Pedra96 <darkraidialga@yahoo.it>
2022-11-15 16:19:50 +0100

    esercizio senza bonus finito,inizio prova bonus

--- js/main.js
+++ js/main.js
@@ -8,12 +8,46 @@ Bonus:
 Aggiungere alla pagina un campo di input testuale e un pulsante “aggiungi”: l’utente inserisce nell’input un prodotto da acquistare e quando clicca sul pulsante, il testo viene aggiunto alla lista.
 Super Bonus:
 Aggiungere a fianco ad ogni elemento della lista un pulsante “elimina”: quando l’utente clicca su uno di questi pulsanti, l’elemento corrispondente viene rimosso dalla lista*/
+/*-------------------------------------------------------------
+ esempio di while loop
+let i=0;
+while(i<arrayListaSpesa.length){
+    console.log(arrayListaSpesa[i]);
+    i++;
+}
+*/
 
-let lista=document.getElementById("AggiungeElemento");
+/* esempio di do... while loop
+let i=0;
+do{
+    console.log(arrayListaSpesa[i]);
+    i++;
+}while(i<arrayListaSpesa.length);
+----------------------------------------------------------------*/
+
+let contenitoreListaSpesa=document.getElementById("listaSpesa");
+let i=0;
+
+let arrayListaSpesa=["3x latte","Spaghetti","Acqua naturale","Cibo per cani"];
+
+do{
+    contenitoreListaSpesa.innerHTML += '<li id="elementoLista'+i+'" class="mr-2">'+arrayListaSpesa[i]+'<button type="button" class="btn btn-primary " id="RemoveSelf'+i+'">Rimuovi</button></li>';
+    i++;
+}while(i<arrayListaSpesa.length);
+
+
+for(let j=0;j<arrayListaSpesa.length;j++){
+   let removeSelf=document.getElementById("RemoveSelf"+j);
+   let listaelemento=document.getElementById("elementoLista"+j);
+    console.log(removeSelf);
+    removeSelf.addEventListener("click", function(){
+        listaelemento.remove();
+    });
+
+}
+
+/*let lista=document.getElementById("AggiungeElemento");
 
 function aggiungiElemento(){
-    lista.innerHTML+="<li>primo elemento</li>";
-}
-function rimuoviElemento(){
-    lista.innerHTML="";
-}
+    lista.innerHTML+='<li>3x latte<button type="button" class="btn btn-primary" onClick=rimuoviElemento()>Rimuovi</button></li>';
+}*/
